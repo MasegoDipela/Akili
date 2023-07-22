7eefeff1cb05b452c0597e1600656f42d04d7301
Masego Dipela <masegolxve@gmail.com>
2023-07-22 17:10:11 +0200

    Updated the Dashboard Page Added the foundations of a Navbar and Sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Sidebar = () => {
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full
+        bg-[#111827] text-white">
+            <div className="px-3 py-2 flex-1">
+                <Link href="/dashbaord" className="flex
+                items-center pl-3 mb-14">
+                    Logo
+                    <div className="relative w-8 h-8 mr-4">
+                        <Image
+                           fill
+                           alt="Logo"
+                           src="/Logo.png" 
+                        />
+
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
